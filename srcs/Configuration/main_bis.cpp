f049071959373cd2565230dfe732d87f654a92e4
TastyBao <erard.bastien@gmail.com>
2023-11-17 17:00:14 +0100

    Je continue Thomas

--- srcs/Configuration/main_bis.cpp
+++ srcs/Configuration/main_bis.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "ConfigCheck.hpp"
 
 int	main(int argc, char *argv[])
 {
@@ -9,7 +10,7 @@ int	main(int argc, char *argv[])
 	}
 	try
 	{
-		blabla
+		ConfigCheck	Config(argv[1]);
 	}
 	catch (std::exception& e)
 	{
